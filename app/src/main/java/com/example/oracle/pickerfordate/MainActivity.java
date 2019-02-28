0b8e3e228a7dd874f7eae0b512d79584452c1916
RickyRenardi <renardiricky@gmail.com>
2019-02-28 17:59:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/oracle/pickerfordate/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.oracle.pickerfordate;
+
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showDatePicker(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(),getString(R.string.datePicker));
+
+    }
+}
